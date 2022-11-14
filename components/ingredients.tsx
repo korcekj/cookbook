f90eb5ed9304d6f9d2b6bd1c33cb7e3625540bb8
Ján Korček <korcekdevelopment@gmail.com>
2022-11-14 17:54:02 +0100

    feat: add ingredient item clickable

--- components/ingredients.tsx
+++ components/ingredients.tsx
@@ -1,9 +1,11 @@
 import type { FC } from 'react';
 import type { Ingredient } from '@lib/sanity.schema';
 
-import { useState, useCallback, useMemo } from 'react';
+import React, { useState, useCallback, useMemo } from 'react';
 import { toFraction } from '@lib/utils';
+import classNames from 'classnames';
 
+import { CheckCircleIcon } from '@heroicons/react/outline';
 import { UsersIcon } from '@heroicons/react/solid';
 
 interface IngredientsProps {
@@ -76,6 +78,8 @@ const IngredientItem: FC<IngredientItemProps> = ({
   quantity = 0,
   unit,
 }) => {
+  const [checked, setChecked] = useState(false);
+
   const value = useMemo(() => {
     return (quantity * portion.next) / portion.prev;
   }, [quantity, portion]);
@@ -83,12 +87,27 @@ const IngredientItem: FC<IngredientItemProps> = ({
   return (
     <li
       key={title}
-      className='flex items-center space-x-2 p-2 border border-gray-100 rounded'
+      className={classNames(
+        'flex',
+        'items-center',
+        'space-x-2',
+        'p-2',
+        'border',
+        'border-gray-100',
+        'rounded',
+        'hover:bg-gray-100',
+        'hover:cursor-pointer',
+        {
+          'bg-gray-100': checked,
+        }
+      )}
+      onClick={() => setChecked((v) => !v)}
     >
-      <input
-        type='checkbox'
-        className='w-4 h-4 text-gray-600 bg-gray-100 rounded border-gray-300 focus:ring-gray-600 focus:ring-2'
-      />
+      {checked ? (
+        <CheckCircleIcon className='w-5 h-5 text-gray-900' />
+      ) : (
+        <CheckCircleIcon className='w-5 h-5 text-gray-300' />
+      )}
       <div className='text-base font-light text-gray-900 space-x-1 overflow-x-auto'>
         <span className='font-medium'>{title}</span>
         <span>
