6d59402ca5bdec7d7d2e44e9345136e229499ad2
Ján Korček <korcekdevelopment@gmail.com>
2023-08-30 20:00:42 +0200

    refactor: elements order

--- src/lib/components/Section.svelte
+++ src/lib/components/Section.svelte
@@ -10,18 +10,16 @@
 	let className = '';
 </script>
 
-<div class={twMerge('flex items-center space-x-1 rounded-lg py-2 px-4', className)}>
+<h2 class={twMerge('flex items-center space-x-1 rounded-lg py-2 px-4', className)}>
 	<Hash class="w-4 h-4" />
-	<a href={`#${name}`}>
-		<h2 class={twMerge('pointer-events-none text-2xl', titleClass)}>
-			<slot name="title"
-				><span class="font-bold"><slot name="title-bold" /> </span><span class="font-light">
-					<slot name="title-light" />
-				</span></slot
-			>
-		</h2>
+	<a href={`#${name}`} class={twMerge('text-2xl', titleClass)}>
+		<slot name="title"
+			><span class="font-bold"><slot name="title-bold" /> </span><span class="font-light">
+				<slot name="title-light" />
+			</span></slot
+		>
 	</a>
-</div>
+</h2>
 
 <div id={name}>
 	<slot />
