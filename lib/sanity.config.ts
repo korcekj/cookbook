b86cd61472c5c2ec6a3525255c7de1032cc5973f
Ján Korček <korcekdevelopment@gmail.com>
2023-08-30 14:18:02 +0200

    build: previous version

--- lib/sanity.config.ts
+++
@@ -1,8 +0,0 @@
-import type { ClientConfig } from 'next-sanity';
-
-export const config: ClientConfig = {
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  apiVersion: process.env.NEXT_PUBLIC_SANITY_VERSION,
-  useCdn: process.env.NODE_ENV === 'production',
-};
--- lib/sanity.ts
+++
@@ -1,9 +0,0 @@
-import type { SanityImageSource } from '@sanity/image-url/lib/types/types';
-
-import { createClient, createImageUrlBuilder } from 'next-sanity';
-import { config } from './sanity.config';
-
-export const sanityClient = createClient(config);
-
-export const urlFor = (source: SanityImageSource) =>
-  createImageUrlBuilder(config).image(source).auto('format').fit('max');
