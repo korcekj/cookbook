20e359c4a4eccdf13fd34f5cad290ab464d57dda
Ján Korček <korcekdevelopment@gmail.com>
2023-08-31 15:05:43 +0200

    feat: add opacity to step

--- src/lib/components/Step.svelte
+++ src/lib/components/Step.svelte
@@ -4,10 +4,14 @@
 	export { className as class };
 
 	let className = '';
+	let checked = false;
 </script>
 
-<li class={twMerge(className)}>
-	<p class="leading-relaxed">
-		<slot />
-	</p>
+<li class:opacity-50={checked} class={twMerge(className)}>
+	<label class="w-full">
+		<p class="leading-relaxed">
+			<slot />
+		</p>
+		<input type="checkbox" class="hidden" bind:checked />
+	</label>
 </li>
