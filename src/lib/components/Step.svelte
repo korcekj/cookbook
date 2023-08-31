18dbc9690e283d9ec946c604215a574482c33301
Ján Korček <korcekdevelopment@gmail.com>
2023-08-31 14:44:21 +0200

    feat: add stepper

---
+++ src/lib/components/Step.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { twMerge } from 'tailwind-merge';
+
+	export { className as class };
+
+	let className = '';
+</script>
+
+<li class={twMerge(className)}>
+	<p class="leading-relaxed">
+		<slot />
+	</p>
+</li>
---
+++ src/lib/components/Steps.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { twMerge } from 'tailwind-merge';
+
+	export { className as class };
+
+	let className = '';
+</script>
+
+<ol role="list" class={twMerge('stepper not-prose', className)}>
+	<slot />
+</ol>
