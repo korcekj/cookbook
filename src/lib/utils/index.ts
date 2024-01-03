8f577caf0a1916c0b89d28cd0759d28a6fd54b10
Ján Korček <korcekdevelopment@gmail.com>
2024-01-03 17:23:19 +0100

    feat: add pairing functionality

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -7,3 +7,11 @@ export const url = (path: string) => {
 export const slugify = (str: string) => {
 	return deburr(str).toLowerCase().replace(/ +/g, '-');
 };
+
+export const toPairs = <T extends any[]>(arr: T): T[] => {
+	if (arr.length % 2 !== 0) return [];
+	return arr.reduce(
+		(acc, current, i, all) => (i % 2 === 0 ? acc : [...acc, [all[i - 1], current]]),
+		[]
+	);
+};
